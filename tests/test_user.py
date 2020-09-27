edbf24f280daa65d59940473762866fe19c60ed4
PerisOduol618 <oduolpepe618@gmail.com>
2020-09-27 03:08:55 +0300

    Add tests for the class User

---
+++ tests/test_user.py
@@ -0,0 +1,18 @@
+import unittest
+from app.models import User
+
+class UserModelTest(unittest.TestCase):
+
+    def setUp(self):
+        self.new_user = User(password = 'mango')
+    
+    def test_password_setter(self):
+        self.assertTrue(self.new_user.pass_secure is not None)
+    
+    def test_no_access_password(self):
+        with self.assertRaises(ArithmeticError):
+            self.new_user.password
+
+
+    def test_password_verfication(self):
+        self.assertTrue(self.new_user.verify_password('mango'))     
