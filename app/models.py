ffbaf1affbb1eb87cf415ff198b9a6885d482560
PerisOduol618 <oduolpepe618@gmail.com>
2020-09-28 16:45:36 +0300

    Add a class  comments

--- app/models.py
+++ app/models.py
@@ -64,22 +64,22 @@ class Blog(db.Model):
     def __repr__(self):
         return f'Blogs {self.blog_title}'
 
-class Comment(db.Model):
-    __tablename__ = 'comments'
-    id = db.Column(db.Integer, primary_key=True)
-    comment = db.Column(db.Text())
-    blog_id = db.Column(db.Integer, db.ForeignKey('blogs.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    date = db.Column(db.DateTime, default=datetime.utcnow)
-    def save_comment(self):
-        db.session.add(self)
-        db.session.commit()
-    @classmethod
-    def get_comments(cls, blog_id):
-        comments = Comment.query.filter_by(blog_id=blog_id).all()
-        return comments
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
-    def __repr__(self):
-        return f'Comments: {self.comment}'
+# class Comment(db.Model):
+#     __tablename__ = 'comments'
+#     id = db.Column(db.Integer, primary_key=True)
+#     comment = db.Column(db.Text())
+#     blog_id = db.Column(db.Integer, db.ForeignKey('blogs.id'))
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+#     date = db.Column(db.DateTime, default=datetime.utcnow)
+#     def save_comment(self):
+#         db.session.add(self)
+#         db.session.commit()
+#     @classmethod
+#     def get_comments(cls, blog_id):
+#         comments = Comment.query.filter_by(blog_id=blog_id).all()
+#         return comments
+#     def delete(self):
+#         db.session.delete(self)
+#         db.session.commit()
+#     def __repr__(self):
+#         return f'Comments: {self.comment}'
