105934787362b3deeb75b989d2fb7b59e4056dbc
PerisOduol618 <oduolpepe618@gmail.com>
2020-09-28 21:53:36 +0300

    Add call the wtf form

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,3 +6,14 @@ class UpdateProfile(FlaskForm):
     bio = TextAreaField('Bio.',validators = [Required()])
     submit = SubmitField('Submit')
 
+
+class BlogForm(FlaskForm):
+    title_blog = StringField('Title')
+    description = TextAreaField('Write a Description', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Write a comment', validators=[Required()])
+    submit = SubmitField('Comment')
+
+
